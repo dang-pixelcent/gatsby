bcca7e226603b3ddebae5d10cebc03f360c9b0df
minhdat204 <huynhminhdat1506@gmail.com>
2025-06-12 11:04:05 +0700

    Add DynamicScriptHandler component to manage script initialization for various UI elements

---
+++ src/components/DynamicScriptHandler/index.js
@@ -0,0 +1,48 @@
+// src/components/DynamicScriptHandler/index.js
+
+import { useEffect } from 'react';
+import { useLocation } from "@reach/router";
+
+// 1. Import các handler mới
+import { initializeFAQs } from '../../script-handlers/faq-handler';
+import { initializeSlickCarousel } from '../../script-handlers/slick-carousel-handler';
+import { initializeLoadMore } from '../../script-handlers/load-more-handler';
+import { initializeTabs } from '../../script-handlers/tab-handler';
+import { initializeFormEmbed } from '../../script-handlers/form-embed-handler';
+import { initializeHeightEqualizer } from '../../script-handlers/height-equalizer-handler';
+import { initializeJotForm } from '../../script-handlers/jotform-handler';
+
+// 2. Thêm chúng vào danh sách đăng ký
+const HANDLER_REGISTRY = [
+    initializeFAQs,
+    initializeSlickCarousel,
+    initializeLoadMore,
+    initializeTabs,
+    initializeFormEmbed,
+    initializeHeightEqualizer,
+    initializeJotForm,
+];
+
+const DynamicScriptHandler = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+        const cleanupFunctions = [];
+
+        HANDLER_REGISTRY.forEach(handler => {
+            const cleanup = handler();
+            if (cleanup && typeof cleanup === 'function') {
+                cleanupFunctions.push(cleanup);
+            }
+        });
+
+        return () => {
+            cleanupFunctions.forEach(cleanup => cleanup());
+        };
+
+    }, [location.pathname]);
+
+    return null;
+};
+
+export default DynamicScriptHandler;
