5ee59d5443968b597a8dc17566518335fb61fa22
minhdat204 <huynhminhdat1506@gmail.com>
2025-06-13 18:31:29 +0700

    Add ServiceSlider component with image data and background styling

---
+++ src/components/Blocks/ServiceSlider.js/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ServiceHeroSlider from './ServiceHeroSlider';
+
+
+export function ServiceSlider() {
+    // link các ảnh sẽ được truyền vào component ServiceHeroSlider
+    const imageData = [
+        { src: 'https://agencysitestaging.mystagingwebsite.com/wp-content/uploads/2024/12/new-slider-component-1.jpg', alt: 'slide 1' },
+        { src: 'https://agencysitestaging.mystagingwebsite.com/wp-content/uploads/2024/12/new-component-2.png', alt: 'slide 2' },
+        { src: 'https://agencysitestaging.mystagingwebsite.com/wp-content/uploads/2024/12/new-component-3.png', alt: 'slide 3' },
+    ];
+
+    const backgroundStyle = {
+        background: "no-repeat center/cover url('https://www.wellnessclinicmarketing.com/wp-content/uploads/2024/12/slider-iframe-bg.png')"
+    };
+
+    return (
+        <div className="iframe-slider" style={backgroundStyle}>
+            <ServiceHeroSlider images={imageData} />
+        </div>
+    );
+}
