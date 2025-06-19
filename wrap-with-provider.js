bc1a8f60bb7c01d621c7e3550b8fc72e5dfddcb7
minhdat204 <huynhminhdat1506@gmail.com>
2025-06-19 13:21:58 +0700

    feat: Wrap Provider with MobileMenuProvider for improved mobile menu state management

--- wrap-with-provider.js
+++ wrap-with-provider.js
@@ -2,12 +2,20 @@ import React from "react"
 import { Provider } from "react-redux"
 
 import createStore from "./src/state/createStore"
+import { MobileMenuProvider } from '@context/MobileMenuContext';
 
+const store = createStore()
 // eslint-disable-next-line react/display-name,react/prop-types
 export default ({ element }) => {
   // Instantiating store in `wrapRootElement` handler ensures:
   //  - there is fresh store for each SSR page
   //  - it will be called only once in browser, when React mounts
-  const store = createStore()
-  return <Provider store={store}>{element}</Provider>
+
+  return (
+    <MobileMenuProvider>
+      <Provider store={store}>
+        {element}
+      </Provider>
+    </MobileMenuProvider>
+  )
 }
