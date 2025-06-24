cfad64b3dc264f4022b92d584f549c10c0cbc520
minhdat204 <huynhminhdat1506@gmail.com>
2025-06-24 15:17:10 +0700

    feat: Refactor pagination logic to handle query parameters in basePath and adjust siblingCount

--- src/components/Blog/Pagination.js
+++ src/components/Blog/Pagination.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import { usePagination } from '../../hooks/usePagination'; 
+import { usePagination } from '../../hooks/usePagination';
 
 const Pagination = ({ currentPage, numPages, basePath }) => {
     const paginationRange = usePagination({
         currentPage,
         totalPages: numPages,
-        siblingCount: 2, // Thay đổi từ 2 về 1
-        boundaryCount: 1, // Có thể tùy chỉnh
+        siblingCount: 1,
+        boundaryCount: 1,
     });
 
     if (numPages <= 1) {
@@ -16,36 +16,41 @@ const Pagination = ({ currentPage, numPages, basePath }) => {
 
     const isFirst = currentPage === 1;
     const isLast = currentPage === numPages;
-    const prevPage = currentPage - 1 === 1 ? basePath : `${basePath}/page/${currentPage - 1}`;
-    const nextPage = `${basePath}/page/${currentPage + 1}`;
+
+    // Sửa đổi ở đây: Kiểm tra xem basePath có chứa query param không
+    const getPageLink = (page) => {
+        if (basePath.includes('?')) {
+            // Dành cho trang tìm kiếm (ví dụ: /blogs/search?q=abc&)
+            return `${basePath}page=${page}`;
+        }
+        // Dành cho trang blog chính
+        return page === 1 ? basePath : `${basePath}/page/${page}`;
+    };
+
+    const prevPage = getPageLink(currentPage - 1);
+    const nextPage = getPageLink(currentPage + 1);
 
     return (
         <div className="page-navigation">
-            {/* Nút Previous */}
             {!isFirst && (
                 <Link className="button" to={prevPage} rel="prev">&lt;</Link>
             )}
 
             {paginationRange.map((page, index) => {
-                // Nếu là dấu "...", render ra một thẻ span không click được
                 if (typeof page === 'string') {
                     return <span key={`dots-${index}`} className="button dots">&hellip;</span>;
                 }
-
-                // Nếu là một số, render ra Link
-                const pageLink = page === 1 ? basePath : `${basePath}/page/${page}`;
                 return (
                     <Link
                         key={`pagination-number-${page}`}
                         className={`button ${page === currentPage ? 'active' : ''}`}
-                        to={pageLink}
+                        to={getPageLink(page)}
                     >
                         {page}
                     </Link>
                 );
             })}
 
-            {/* Nút Next */}
             {!isLast && (
                 <Link className="button" to={nextPage} rel="next">&gt;</Link>
             )}
