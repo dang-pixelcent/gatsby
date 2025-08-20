42cebb7250e9eaaee5bf2ae6039b4cfc3bc04f31
minhdat204 <huynhminhdat1506@gmail.com>
2025-08-20 17:58:40 +0700

    feat: Update image URLs in ServiceSlider to use the correct domain

--- src/components/Blocks/ServiceSlider.js/index.js
+++ src/components/Blocks/ServiceSlider.js/index.js
@@ -5,9 +5,9 @@ import ServiceHeroSlider from './ServiceHeroSlider';
 function ServiceSlider() {
     // link các ảnh sẽ được truyền vào component ServiceHeroSlider
     const imageData = [
-        { src: 'https://agencysitestaging.mystagingwebsite.com/wp-content/uploads/2024/12/new-slider-component-1.jpg', alt: 'slide 1' },
-        { src: 'https://agencysitestaging.mystagingwebsite.com/wp-content/uploads/2024/12/new-component-2.png', alt: 'slide 2' },
-        { src: 'https://agencysitestaging.mystagingwebsite.com/wp-content/uploads/2024/12/new-component-3.png', alt: 'slide 3' },
+        { src: 'https://a.wellnessclinicmarketing.com/wp-content/uploads/2024/12/new-slider-component-1.jpg', alt: 'slide 1' },
+        { src: 'https://a.wellnessclinicmarketing.com/wp-content/uploads/2024/12/new-component-2.png', alt: 'slide 2' },
+        { src: 'https://a.wellnessclinicmarketing.com/wp-content/uploads/2024/12/new-component-3.png', alt: 'slide 3' },
     ];
 
     const backgroundStyle = {
