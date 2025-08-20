56710507b43f822939c327a9591eba63c1f47d9e
minhdat204 <huynhminhdat1506@gmail.com>
2025-08-20 14:36:03 +0700

    feat: Restore useStaticQuery implementation in useQuizData hook

--- src/components/Quiz/data/useQuizData.js
+++ src/components/Quiz/data/useQuizData.js
@@ -1,16 +1,15 @@
 import { graphql, useStaticQuery } from 'gatsby';
 
 export const useQuizData = () => {
-    // const data = useStaticQuery(graphql`
-    //     query SharedQuizData {
-    //         cms {
-    //             formSnippets {
-    //                 hrtWomenQuiz: scheduleform 
-    //             }
-    //         }
-    //     }
-    // `);
+    const data = useStaticQuery(graphql`
+        query SharedQuizData {
+            cms {
+                formSnippets {
+                    hrtWomenQuiz: scheduleform 
+                }
+            }
+        }
+    `);
 
-    // return data.cms.formSnippets.hrtWomenQuiz;
-    return null
+    return data.cms.formSnippets.hrtWomenQuiz;
 };
