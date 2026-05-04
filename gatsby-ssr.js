37ea4ffbcef5cc79b184a461c317055c4ac861b3
minhdat204 <huynhminhdat1506@gmail.com>
2026-05-04 12:21:22 +0700

    feat(gatsby-ssr): enhance GTM integration by adding head components for improved tracking

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,10 +3,37 @@ import wrapWithProvider from './wrap-with-provider';
 
 export const wrapRootElement = wrapWithProvider;
 
-// Hàm này chèn code vào *ngay sau* thẻ <body> mở
-export const onRenderBody = ({ setPreBodyComponents }) => {
-  setPreBodyComponents([ // <-- Đã đổi thành setPreBodyComponents
-    // Đây là GTM noscript iframe (từ 'bodyOpenHtml')
+// Render cứng GTM/Gtag trong SSR để tag assistant thấy ngay trong HTML source
+export const onRenderBody = ({ setHeadComponents, setPreBodyComponents }) => {
+  setHeadComponents([
+    <script
+      key="gtag-lib"
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=UA-179011612-1"
+    />,
+    <script
+      key="gtag-inline"
+      dangerouslySetInnerHTML={{
+        __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-179011612-1');
+gtag('config', 'AW-578323724');`,
+      }}
+    />,
+    <script
+      key="gtm-inline"
+      dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KGBVQVH');`,
+      }}
+    />,
+  ]);
+
+  setPreBodyComponents([
     <noscript
       key="gtm-noscript"
       dangerouslySetInnerHTML={{
