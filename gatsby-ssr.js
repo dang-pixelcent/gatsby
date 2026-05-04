8c27af7206b1b8101baeca4a575629262f569161
minhdat204 <huynhminhdat1506@gmail.com>
2026-05-04 14:23:01 +0700

    refactor(gatsby-ssr): clean up GTM script formatting for improved readability

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,7 +3,6 @@ import wrapWithProvider from './wrap-with-provider';
 
 export const wrapRootElement = wrapWithProvider;
 
-// Render cứng GTM/Gtag trong SSR để tag assistant thấy ngay trong HTML source
 export const onRenderBody = ({ setHeadComponents, setPreBodyComponents }) => {
   setHeadComponents([
     <script
@@ -14,21 +13,13 @@ export const onRenderBody = ({ setHeadComponents, setPreBodyComponents }) => {
     <script
       key="gtag-inline"
       dangerouslySetInnerHTML={{
-        __html: `window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'UA-179011612-1');
-gtag('config', 'AW-578323724');`,
+        __html: `window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'UA-179011612-1');\ngtag('config', 'AW-578323724');`,
       }}
     />,
     <script
       key="gtm-inline"
       dangerouslySetInnerHTML={{
-        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KGBVQVH');`,
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':\nnew Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\nj=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\n'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\n})(window,document,'script','dataLayer','GTM-KGBVQVH');`,
       }}
     />,
   ]);
@@ -37,12 +28,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     <noscript
       key="gtm-noscript"
       dangerouslySetInnerHTML={{
-        __html: `
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KGBVQVH"
-                  height="0" width="0"
-                  style="display:none;visibility:hidden">
-          </iframe>
-        `,
+        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KGBVQVH" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
       }}
     />,
   ]);
